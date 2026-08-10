fbb36d9bbe41a5798371528e07e293e56bc6d164
Min <kendamamin1480@gmail.com>
2026-08-10 00:21:27 -1000

    One more line break

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,6 +120,7 @@ export default async function LeaderboardPage() {
       <p style={{ fontSize: '0.8rem', color: '#777', marginTop: 0, marginBottom: '1.5rem' }}>
         Updated on {lastUpdated}
         <br />
+        <br />
         Updates daily at midnight
       </p>
 
