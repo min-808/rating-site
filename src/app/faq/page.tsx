2822c4a5b4facf8ee103fbdce6eff921efd0d58e
Min <64472558+min-808@users.noreply.github.com>
2026-08-18 19:11:48 -1000

    Changed friend ID to code block

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -27,7 +27,7 @@ export default function FAQ() {
       question: "i'm a maimai player from hawaii. how do i get listed on this website?",
       answer: (
         <>
-          first, add me as a friend on maimai either by playing with me irl, or by going to the <a href="https://maimaidx-eng.com/maimai-mobile/friend/search/">friend site</a> and adding me with my friend code: 101142379434455. optionally, you can let me know that you friend requested me by messaging me on discord <code style={codeStyle}>@waitaamin</code>. once you've been added to my friends list, you'll then be scraped into the site on the next refresh.
+          first, add me as a friend on maimai either by playing with me irl, or by going to the <a href="https://maimaidx-eng.com/maimai-mobile/friend/search/">friend site</a> and adding me with my friend code: <code style={codeStyle}>101142379434455</code>. optionally, you can let me know that you friend requested me by messaging me on discord <code style={codeStyle}>@waitaamin</code>. once you've been added to my friends list, you'll then be scraped into the site on the next refresh.
         </>
       ),
     },
