81254916a453c44e69a4e19e6696cf19eb6b2a50
Min <kendamamin1480@gmail.com>
2026-08-19 01:55:42 -1000

    open new tab upon clicking on link

--- .next/dev/server/chunks/ssr/[root-of-the-server]__1h02u2t._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__1h02u2t._.js
@@ -49,6 +49,8 @@ function faq() {
                     "add me as a friend on maimai either by playing with me irl, or by going to the ",
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
                         href: "https://maimaidx-eng.com/maimai-mobile/friend/search/",
+                        target: "_blank",
+                        rel: "noopener noreferrer",
                         children: "friend site"
                     }, void 0, false, {
                         fileName: "[project]/src/app/faq/page.tsx",
@@ -62,7 +64,7 @@ function faq() {
                     }, void 0, false, {
                         fileName: "[project]/src/app/faq/page.tsx",
                         lineNumber: 34,
-                        columnNumber: 205
+                        columnNumber: 247
                     }, this),
                     ". optionally, you can let me know that you friend requested me by messaging me on discord ",
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("code", {
@@ -71,7 +73,7 @@ function faq() {
                     }, void 0, false, {
                         fileName: "[project]/src/app/faq/page.tsx",
                         lineNumber: 34,
-                        columnNumber: 341
+                        columnNumber: 383
                     }, this),
                     ". once you've been added to my friends list, you'll then be scraped into the site on the next refresh"
                 ]
--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -31,7 +31,7 @@ export default function faq() {
       question: "i'm a maimai player from hawaii. how do i get listed on this website?",
       answer: (
         <>
-          add me as a friend on maimai either by playing with me irl, or by going to the <a href="https://maimaidx-eng.com/maimai-mobile/friend/search/">friend site</a> and adding me with my friend code: <code style={codeStyle}>101142379434455</code>. optionally, you can let me know that you friend requested me by messaging me on discord <code style={codeStyle}>@waitaamin</code>. once you've been added to my friends list, you'll then be scraped into the site on the next refresh
+          add me as a friend on maimai either by playing with me irl, or by going to the <a href="https://maimaidx-eng.com/maimai-mobile/friend/search/" target="_blank" rel="noopener noreferrer">friend site</a> and adding me with my friend code: <code style={codeStyle}>101142379434455</code>. optionally, you can let me know that you friend requested me by messaging me on discord <code style={codeStyle}>@waitaamin</code>. once you've been added to my friends list, you'll then be scraped into the site on the next refresh
         </>
       ),
     },
