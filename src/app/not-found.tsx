de0d9c7dff2ca651e4a9a46fa470d920a3ef373d
Min <kendamamin1480@gmail.com>
2026-08-20 01:58:42 -1000

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'hi',
+  description: 'hi',
+};
+
+export default function hi() {
+
+  return (
+    <main style={{ padding: '0 2rem 2rem 2rem', maxWidth: '800px', margin: '0 auto', fontFamily: 'sans-serif' }}>
+      <p style={{ margin: 0, lineHeight: '1.5', color: '#333' }}>
+        404 - not found
+      </p>
+    </main>
+  );
+}
