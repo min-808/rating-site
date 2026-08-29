7ff07c3a273d51578e650f4d78f0b810c6a34663
Min <kendamamin1480@gmail.com>
2026-08-28 17:10:23 -1000

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import Link from 'next/link';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
   children,
@@ -32,6 +33,7 @@ export default function RootLayout({
         </div>
         
         {children}
+        <Analytics />
 
         <footer style={{
           textAlign: 'center',
