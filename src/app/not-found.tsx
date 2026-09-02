fd1545e8e7123aaa7a072d99baf390f6deca1646
Min <kendamamin1480@gmail.com>
2026-09-02 12:12:12 -1000

    Update 404 not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,22 @@ import Link from 'next/link';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'hi',
-  description: 'hi',
+  title: '404 - HI Maimai',
+  description: 'you stumbled on a page that doesn\'t exist!',
 };
 
-export default function hi() {
+export default function NotFound() {
 
   return (
     <main style={{ padding: '0 2rem 2rem 2rem', maxWidth: '800px', margin: '0 auto', fontFamily: 'sans-serif' }}>
+      <p style={{ margin: 0, lineHeight: '1.5', color: '#333', marginBottom: '1rem' }}>
+        404 not found
+      </p>
+      <p style={{ margin: 0, lineHeight: '1.5', color: '#333', marginBottom: '1rem' }}>
+        you stumbled on a page that doesn't exist!
+      </p>
       <p style={{ margin: 0, lineHeight: '1.5', color: '#333' }}>
-        404 - not found
+        <Link href="/">go back to the leaderboard</Link>
       </p>
     </main>
   );
